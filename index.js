c218d66ab3dd42fc63881ab5e24004b8780d19ac
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-07-16 21:47:37 -0300

    Reindent file with two spaces

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ const app = express();
 app.get("/", (req, res) =>  res.status(200).json("TP fallas I"));
 
 app.get("/motor", async (req, res) => {
-	const ruleEngine = new RuleEngine();
-	const { rpm, tflp, mp, ph, pea, vps } = req.query;
-	const message = await ruleEngine.process(
-		parseInt(rpm),
-		tflp.toUpperCase(),
-		mp.toUpperCase(),
-		parseInt(ph),
-		parseInt(pea),
-		vps.toUpperCase()
-	);
-	return res.status(200).json(message);
+  const ruleEngine = new RuleEngine();
+  const { rpm, tflp, mp, ph, pea, vps } = req.query;
+  const message = await ruleEngine.process(
+    parseInt(rpm),
+    tflp.toUpperCase(),
+    mp.toUpperCase(),
+    parseInt(ph),
+    parseInt(pea),
+    vps.toUpperCase()
+  );
+  return res.status(200).json(message);
 });
 
 export { app };
