78990e6e7103b3b2c63ff449a2a6983cd6ec49ee
llmanuel <llauromanuel@gmail.com>
2020-08-01 12:41:55 -0300

    Attend comments

--- src/models/rule_engine.js
+++ src/models/rule_engine.js
@@ -1,6 +1,7 @@
 import Engine from 'json-rules-engine';
+import { events } from './events';
 import { rules } from "./rules";
-import { factsBuilder } from "./facts";
+import { factsBuilder, FactsSchema } from "./facts";
 
 export class RuleEngine {
   resultMessage = '';
@@ -17,7 +18,7 @@ export class RuleEngine {
       if (detail.includes('invalido')) {
         self.resultMessage = `Parametros inválidos. ${detail}`;
       } else {
-        self.resultMessage = `El pozo está sucio. ${detail}`;
+        self.resultMessage = `${events.dirty.message}. ${detail}`;
       }
     });
   }
@@ -29,8 +30,8 @@ export class RuleEngine {
           case 'equal':
             return `${condition.fact} tiene que ser ${condition.value} pero fue ${condition.factResult}`;
           case 'greaterThan':
-            if (condition.fact === 'hydrostaticPressure') {
-              return 'hydrostaticPressure tiene que ser mayor que annularSpacePressure';
+            if (condition.fact === FactsSchema.hydrostaticPressure.name) {
+              return `${FactsSchema.hydrostaticPressure.name} tiene que ser mayor que ${FactsSchema.annularSpacePressure.name}`;
             }
             return `${condition.fact} de ${condition.factResult} es muy baja`;
           case 'in':
