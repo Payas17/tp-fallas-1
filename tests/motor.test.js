b03b1fcf2d990871e464cf9f9c9b1f10a2dcf918
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-08-03 04:25:15 -0300

    Fix indentation

--- tests/motor.test.js
+++ tests/motor.test.js
@@ -39,7 +39,11 @@ describe("motor", () => {
       pea: 50,
       vps: FactsSchema.stringPowerVariance.values.VPN
     });
-    expect(request.body).toEqual(`${events.dirty.message}. drillingFlowMud tiene que ser LPT pero fue LPL`);
+    expect(
+      request.body
+    ).toEqual(
+      `${events.dirty.message}. drillingFlowMud tiene que ser LPT pero fue LPL`
+    );
   });
 
   it("returns that is not clean if drillingMethod is not MPR", async () => {
@@ -51,10 +55,14 @@ describe("motor", () => {
       pea: 50,
       vps: FactsSchema.stringPowerVariance.values.VPN
     });
-    expect(request.body).toEqual(`${events.dirty.message}. drillingMethod tiene que ser MPR pero fue MPD`);
+    expect(
+      request.body
+    ).toEqual(
+      `${events.dirty.message}. drillingMethod tiene que ser MPR pero fue MPD`
+    );
   });
 
-  it("returns that is not clean if annularSpacePressure is grater than the hydrostaticPressure", async () => {
+  it("returns dirty if annularSpacePressure is grater than the hydrostaticPressure", async () => {
     const ph = 90;
     const pea = 100;
     const request = await client(app).get(ENDPOINT).query({
@@ -65,7 +73,11 @@ describe("motor", () => {
       pea,
       vps: FactsSchema.stringPowerVariance.values.VPN
     });
-    expect(request.body).toEqual(`${events.dirty.message}. hydrostaticPressure tiene que ser mayor que annularSpacePressure`);
+    expect(
+      request.body
+    ).toEqual(
+      `${events.dirty.message}. hydrostaticPressure tiene que ser mayor que annularSpacePressure`
+    );
   });
 
   it("returns that is not clean if stringPowerVariance is not VPN", async () => {
@@ -77,7 +89,11 @@ describe("motor", () => {
       pea: 50,
       vps: FactsSchema.stringPowerVariance.values.VPA
     });
-    expect(request.body).toEqual(`${events.dirty.message}. stringPowerVariance tiene que ser VPN pero fue VPA`);
+    expect(
+      request.body
+    ).toEqual(
+      `${events.dirty.message}. stringPowerVariance tiene que ser VPN pero fue VPA`
+    );
   });
 
   it("returns that is not clean if all rules fail", async () => {
@@ -105,7 +121,11 @@ describe("motor", () => {
       pea: 50,
       vps: "VPE"
     });
-    expect(request.body).toEqual("Parametros inválidos. VPE es un stringPowerVariance invalido");
+    expect(
+      request.body
+    ).toEqual(
+      "Parametros inválidos. VPE es un stringPowerVariance invalido"
+    );
   });
 
 });
